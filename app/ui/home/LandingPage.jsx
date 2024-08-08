89e28da93c91a9df16973d259ab7512985b10bdf
Ridwan <phercerchridwan98@gmail.com>
2024-08-08 15:26:42 +0100

    updated

--- app/ui/home/LandingPage.jsx
+++ app/ui/home/LandingPage.jsx
@@ -57,9 +57,9 @@ const handleSignOut = () => {
 
 
 
-if (isAuthenticated) {
-  return null; // or you can show a loading spinner
-}
+// if (isAuthenticated) {
+//   return null; // or you can show a loading spinner
+// }
 
 
 
